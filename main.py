2a5a9a071b53d0b2b1f91a1279fa1d4b6e49c18c
Negative <negative_amir@yahoo.com>
2016-07-17 23:48:56 +0430

    fix

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ def getUpdates(offset=None, limit=None, timeout=None):
     }
     return json.loads(requests.get(url + 'getUpdates', params=Params).content.decode('utf8'))
 
-def send_msg(chat_id, text, disable_web=None, reply_t=None, reply_markup=None):
+def send_msg(chat_id, text, disable_web=None, reply_to_message_id=None, reply_markup=None):
     Params = {
         'chat_id': chat_id,
         'text': text,
         'parse_mode':'HTML',
         'disable_web_page_preview': disable_web,
-        'reply_to_message_id': reply_t,
+        'reply_to_message_id': reply_to_message_id,
         'reply_markup': reply_markup
     }
     return requests.get(url + 'sendMessage', params=Params)
@@ -59,10 +59,12 @@ def run ():
                         key = json.dumps({'inline_keyboard':[[{'text':'Developer 👓','url':'https://telegram.me/negative'},{'text':'Taylor Team 🔌','url':'https://telegram.me/taylor_team'}]]})
                         send_msg(chat_id,'<b>Taylor Team Development</b>',reply_markup=key)
                     if(command == '/time'):
+                        getUpdates(last_update+1)
                         time = urllib.urlopen('http://api.gpmod.ir/time/').read()
                         data = json.loads(time)
                         en = data['ENtime']
                         msgg = '<b>Time Tehran :</b> {}'.format(en)
-                        send_msg(chat_id,msgg)
+                        send_msg(chat_id,msgg,reply_to_message_id=update['message']['message_id'])
 
 run()
+
